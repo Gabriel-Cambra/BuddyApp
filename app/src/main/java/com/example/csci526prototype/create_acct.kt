d8355e6e82810321caa80026c5ca66b88e50198e
cambrag <cambrag@wwu.edu>
2022-11-21 12:20:45 -0800

    Finished Login Screens

---
+++ app/src/main/java/com/example/csci526prototype/create_acct.kt
@@ -0,0 +1,11 @@
+package com.example.csci526prototype
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class create_acct : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_acct)
+    }
+}
---
+++ app/src/main/java/com/example/csci526prototype/forget_pass.kt
@@ -0,0 +1,11 @@
+package com.example.csci526prototype
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class forget_pass : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forget_pass)
+    }
+}
