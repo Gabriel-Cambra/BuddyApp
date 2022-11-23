cd538cf50968a7b33ea749bd330120c0ba5b753b
cambrag <cambrag@wwu.edu>
2022-11-22 16:51:20 -0800

    More Screens

---
+++ app/src/main/java/com/example/csci526prototype/findSession.kt
@@ -0,0 +1,11 @@
+package com.example.csci526prototype
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class findSession : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.find_session)
+    }
+}
